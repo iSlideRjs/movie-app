e0ede0051b9a7cb33a33415faaa605a887eac194
Nikolai Maslak <maslaknikolai@ya.ru>
2022-10-29 03:57:48 +0800

    Theme state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 import { Header } from './layout/Header';
 import { Footer } from './layout/Footer';
 import { Main } from './layout/Main';
 
 function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const changeTheme = () => {
+    setTheme(
+      theme === 'dark'
+        ? 'light'
+        : 'dark'
+    )
+  }
+
   return (
     <>
-      <Header />
+      <Header
+        theme={theme}
+        onThemeToggle={changeTheme}
+      />
       <Main />
       <Footer />
     </>
